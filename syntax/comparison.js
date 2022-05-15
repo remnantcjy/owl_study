96381da0753be88dcc16be154ddeec6040af7b93
remnantcjy <96381790+remnantcjy@users.noreply.github.com>
2022-05-16 00:40:07 +0900

    생활코딩 Node.js

---
+++ syntax/comparison.js
@@ -0,0 +1,10 @@
+console.log(1==1);  // true
+console.log(1==2);  // false
+console.log(1>2);   // false
+console.log(1<2);   // true
+// 연산자 == 2번 쓰는 이유를 모른다면 === 3번 써라
+// == : 둘이 같냐, === : 둘이 정확하게 같냐
+console.log(1===1);  // true
+console.log(1===2);  // false
+
+// name === 1;
---
+++ syntax/template.js
@@ -0,0 +1,9 @@
+var name = '1_loveyoon';
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco '+name+' laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non  jungyoon proident, sunt in culpa qui officia deserunt mollit anim id est laborum '+name+'.';
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ${name} laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non  jungyoon proident, sunt in culpa qui officia deserunt mollit anim id est laborum ${name}.`;
+
+
+console.log(letter);
