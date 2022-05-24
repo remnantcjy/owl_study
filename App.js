daaae5f750369f2b3cdacaa4fda761108f77a345
remnantcjy <96381790+remnantcjy@users.noreply.github.com>
2022-05-25 02:44:27 +0900

    리액트 기초

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import React, { useState , useEffect} from 'react';
+import React from 'react';
 
 function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = () => setCondition(!condition);
-  useEffect(() =>{
-    console.log(condition);
-  }, [condition])
-  const renderCondition = condition
-    ? 'True'
-    : 'False'
+  const movies = [
+    {title : 'Kossie coder1', year : 2001},
+    {title : 'Kossie coder2', year : 2002},
+    {title : 'Kossie coder3', year : 2003},
+    {title : 'Kossie coder4', year : 2004}
+  ];
+  const renderMovies =movies.map(movie => {
+    return (
+      <div className='movie' key={movie.title}>
+        <div className='movie-title'>{movie.title}</div>
+        <div className='movie-year'>{movie.year}</div>
+      </div>
+    );
+  });
   return (
     <div className="App">
-      <h1>Jungyoon Coder</h1>
-      <div>
-        {renderCondition}
-      </div>
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie list</h1>
+      {renderMovies}
     </div>
   );
 }
