6b53e66d7e2bc63c58e6b5e95ada446742304295
remnantcjy <96381790+remnantcjy@users.noreply.github.com>
2022-05-25 02:16:32 +0900

    리액트 기초

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  const [buttonName, setButtonName] = useState('클릭');
+  const clickButton = () =>{
+    setButtonName('click');
+  }
+
+  return (
+    <div className="App">
+      <h1>Jungyoon Coder</h1>
+      <Counter click="click1" />
+      <Counter click={buttonName} />
+      <Counter />
+      <button onClick={clickButton}>Click</button>
+    </div>
+  );
+}
+
+export default App;
